a44a2981f7e7f69df618edad5b925c65feea466b
Noisekit <noisekit@proton.me>
2024-07-02 15:18:35 +1000

    Disable automatic retries

--- liquidity/ui/src/App.tsx
+++ liquidity/ui/src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { HashRouter } from 'react-router-dom';
 import { ChakraProvider, extendTheme, useColorMode } from '@chakra-ui/react';
 import { Fonts, theme } from '@snx-v3/theme';
-import { DEFAULT_QUERY_STALE_TIME } from '@snx-v3/constants';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { GasSpeedProvider } from '@snx-v3/useGasSpeed';
@@ -19,7 +18,8 @@ const queryClient = new QueryClient({
     queries: {
       retry: false,
       refetchInterval: false, //  if queries needs refetching we should be explicit about it, given erc7412
-      staleTime: DEFAULT_QUERY_STALE_TIME,
+      staleTime: 5 * 60 * 1000,
+      gcTime: 60 * 60 * 1000, // 1h
       refetchOnWindowFocus: false,
       throwOnError: (e) => {
         console.error(e);
