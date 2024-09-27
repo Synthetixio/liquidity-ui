059c6bf859a09db7ac7b426ebfa56549c95b27bc
Noisekit <noisekit@proton.me>
2024-09-27 14:53:37 +1000

    Add deposit function to stataUSDC ABI

--- contracts/importers/importStataUSDC.js
+++ contracts/importers/importStataUSDC.js
@@ -1,3 +1,5 @@
+const extraAbi = ['function deposit(uint256, address, uint16, bool) external'];
+
 export async function importStataUSDC(chainId, preset) {
   if (!preset) {
     throw new Error(`Missing preset`);
@@ -11,14 +13,23 @@ export async function importStataUSDC(chainId, preset) {
           '@synthetixio/v3-contracts/8453-andromeda/CollateralToken_stataBasUSDC.readable.json'
         ),
       ]);
-      return { address: meta.contracts.CollateralToken_stataBasUSDC, abi };
+
+      return {
+        address: meta.contracts.CollateralToken_stataBasUSDC,
+        abi: [...abi, ...extraAbi],
+      };
     }
+    //    CollateralToken_stataBasUSDC.readable.json
     case '84532-andromeda': {
       const [{ default: meta }, { default: abi }] = await Promise.all([
         import('@synthetixio/v3-contracts/84532-andromeda/meta.json'),
         import('@synthetixio/v3-contracts/84532-andromeda/CollateralToken_stataUSDC.readable.json'),
       ]);
-      return { address: meta.contracts.CollateralToken_stataUSDC, abi };
+
+      return {
+        address: meta.contracts.CollateralToken_stataUSDC,
+        abi: [...abi, ...extraAbi],
+      };
     }
     default: {
       throw new Error(`Unsupported deployment ${deployment} for Extras`);
