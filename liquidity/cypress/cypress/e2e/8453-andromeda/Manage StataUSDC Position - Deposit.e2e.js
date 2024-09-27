c2b553daeae543735ff4e6a26675bb317bee59ff
Noisekit <noisekit@proton.me>
2024-09-27 16:38:23 +1000

    Update minimum deposit

--- liquidity/cypress/cypress/e2e/8453-andromeda/Manage StataUSDC Position - Deposit.e2e.js
+++ liquidity/cypress/cypress/e2e/8453-andromeda/Manage StataUSDC Position - Deposit.e2e.js
@@ -3,7 +3,7 @@ import { generatePath } from 'react-router-dom';
 it('should deposit additional USDC collateral', () => {
   cy.connectWallet().then(({ address, privateKey }) => {
     cy.task('setEthBalance', { address, balance: 100 });
-    cy.task('getUSDC', { address: address, amount: 500 });
+    cy.task('getUSDC', { address: address, amount: 1000 });
     cy.task('createAccount', { privateKey }).then((accountId) => {
       cy.wrap(accountId).as('accountId');
     });
@@ -17,7 +17,7 @@ it('should deposit additional USDC collateral', () => {
     cy.visit(`/#${path}?manageAction=deposit&accountId=${accountId}`);
   });
 
-  cy.get('[data-cy="deposit amount input"]').should('exist').type('101');
+  cy.get('[data-cy="deposit amount input"]').should('exist').type('200');
   cy.get('[data-cy="deposit submit"]').should('be.enabled').click();
 
   cy.get('[data-cy="deposit multistep"]')
@@ -27,13 +27,13 @@ it('should deposit additional USDC collateral', () => {
     .and('include.text', 'Wrap USDC into stataUSDc')
     .and('include.text', 'Approve stataUSDc transfer')
     .and('include.text', 'Deposit and Lock stataUSDc')
-    .and('include.text', 'This will deposit and lock 101 USDC to Spartan Council Pool.');
+    .and('include.text', 'This will deposit and lock 200 USDC to Spartan Council Pool.');
 
   cy.get('[data-cy="deposit confirm button"]')
     .should('include.text', 'Execute Transaction')
     .click();
 
-  cy.get('[data-cy="manage stats collateral"]').should('exist').and('include.text', '101 USDC');
+  cy.get('[data-cy="manage stats collateral"]').should('exist').and('include.text', '200 USDC');
 
   cy.get('[data-cy="deposit amount input"]').should('exist').clear().type('69');
   cy.get('[data-cy="deposit submit"]').should('be.enabled').click();
@@ -49,5 +49,5 @@ it('should deposit additional USDC collateral', () => {
     .should('include.text', 'Execute Transaction')
     .click();
 
-  cy.get('[data-cy="manage stats collateral"]').should('exist').and('include.text', '170 USDC');
+  cy.get('[data-cy="manage stats collateral"]').should('exist').and('include.text', '269 USDC');
 });
