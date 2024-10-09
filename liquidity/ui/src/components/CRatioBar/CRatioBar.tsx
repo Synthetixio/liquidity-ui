4ce38622347cc6d86ae53982615dde6d395b7584
Noisekit <noisekit@proton.me>
2024-10-09 16:45:43 +1100

    Fix loading state for Position Stats and CRatio

--- liquidity/ui/src/components/CRatioBar/CRatioBar.tsx
+++ liquidity/ui/src/components/CRatioBar/CRatioBar.tsx
@@ -1,19 +1,18 @@
-import { TriangleDownIcon, TriangleUpIcon, InfoIcon } from '@chakra-ui/icons';
-import { Box, Flex, Progress, Skeleton, Text, Tooltip } from '@chakra-ui/react';
+import { InfoIcon, TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
+import { Box, Flex, Text, Tooltip } from '@chakra-ui/react';
 import { FC } from 'react';
-import { LineWithText } from './LineWithText';
-import { getHealthVariant, getProgressSize, ratioIsMaxUInt } from './CRatioBar.utils';
-import { CRatioBadge } from './CRatioBadge';
 import { CRatioAmount } from './CRatioAmount';
+import { CRatioBadge } from './CRatioBadge';
+import { getHealthVariant, getProgressSize, ratioIsMaxUInt } from './CRatioBar.utils';
+import { LineWithText } from './LineWithText';
 
 export const CRatioBarUi: FC<{
   liquidationCratio: number;
   targetCratio: number;
   currentCRatio: number;
   newCratio?: number;
-  isLoading: boolean;
   hasChanges: boolean;
-}> = ({ targetCratio, liquidationCratio, currentCRatio, newCratio, isLoading, hasChanges }) => {
+}> = ({ targetCratio, liquidationCratio, currentCRatio, newCratio, hasChanges }) => {
   const variant = hasChanges
     ? getHealthVariant({
         targetCratio: targetCratio,
@@ -25,18 +24,6 @@ export const CRatioBarUi: FC<{
         liquidationCratio: liquidationCratio,
         cRatio: currentCRatio,
       });
-
-  const newBarSize = getProgressSize({
-    cRatio: newCratio,
-    targetCratio: targetCratio,
-    liquidationCratio: liquidationCratio,
-  });
-
-  const currentBarSize = getProgressSize({
-    cRatio: currentCRatio,
-    targetCratio: targetCratio,
-    liquidationCratio: liquidationCratio,
-  });
 
   return (
     <Flex flexDir="column" gap="2">
@@ -60,7 +47,7 @@ export const CRatioBarUi: FC<{
       >
         <CRatioAmount value={currentCRatio} />
 
-        {!!hasChanges && (
+        {hasChanges ? (
           <>
             <span>&rarr;</span>
             <Text>
@@ -71,28 +58,30 @@ export const CRatioBarUi: FC<{
                   : `${newCratio.toFixed(2)} %`}
             </Text>
           </>
-        )}
+        ) : null}
 
-        {(hasChanges ? newCratio || 0 : currentCRatio) !== 0 && (
+        {(hasChanges ? newCratio || 0 : currentCRatio) !== 0 ? (
           <CRatioBadge
             cRatio={hasChanges ? newCratio || 0 : currentCRatio}
             liquidationCratio={liquidationCratio}
             targetCratio={targetCratio}
           />
-        )}
+        ) : null}
       </Flex>
 
       <Box position="relative" height="100px" width="full" overflowX="hidden">
         <>
           <LineWithText
             left="25%"
-            text={!isLoading ? `Liquidation < ${liquidationCratio.toFixed(0)}%` : 'Liquidation'}
+            text={
+              liquidationCratio ? `Liquidation < ${liquidationCratio.toFixed(0)}%` : 'Liquidation'
+            }
             tooltipText="Point at which your Position gets liquidated"
           />
           <LineWithText
             left="75%"
             text={
-              !isLoading
+              targetCratio
                 ? `Borrowing Ratio ${
                     ratioIsMaxUInt(targetCratio) ? 'Infinite' : targetCratio.toFixed(0)
                   }%`
@@ -101,44 +90,6 @@ export const CRatioBarUi: FC<{
             tooltipText="Min point at which you can borrow assets"
           />
         </>
-        <Skeleton
-          variant={variant}
-          top={0}
-          bottom={0}
-          height="12px"
-          position="absolute"
-          margin="auto"
-          width="100%"
-          isLoaded={!isLoading}
-        >
-          <Progress
-            variant={
-              currentBarSize < newBarSize && !(currentBarSize >= 100 && newBarSize > 100)
-                ? `update-${variant}`
-                : variant
-            }
-            top={0}
-            bottom={0}
-            height="12px"
-            position="absolute"
-            margin="auto"
-            left="0"
-            width="100%"
-            value={Math.min(newBarSize, currentBarSize)}
-          />
-          <Progress
-            variant={currentBarSize >= newBarSize ? `update-${variant}` : variant}
-            top={0}
-            bottom={0}
-            height="12px"
-            position="absolute"
-            margin="auto"
-            width="100%"
-            left={`${Math.min(newBarSize, currentBarSize)}%`}
-            display={newCratio === 0 ? 'none' : 'block'}
-            value={Math.abs(newBarSize - currentBarSize)}
-          />
-        </Skeleton>
         <Box
           bg={variant}
           height="12px"
@@ -153,7 +104,7 @@ export const CRatioBarUi: FC<{
           margin="auto"
           display={newCratio === 0 ? 'none' : 'block'}
         >
-          {currentCRatio > 0 && !isLoading && (
+          {currentCRatio > 0 ? (
             <>
               <TriangleDownIcon
                 position="absolute"
@@ -170,7 +121,7 @@ export const CRatioBarUi: FC<{
                 color={variant}
               />
             </>
-          )}
+          ) : null}
         </Box>
       </Box>
     </Flex>
@@ -182,16 +133,14 @@ export const CRatioBar: FC<{
   targetCratio?: number;
   currentCRatio?: number;
   newCratio?: number;
-  isLoading: boolean;
   hasChanges: boolean;
-}> = ({ newCratio, currentCRatio, targetCratio, liquidationCratio, isLoading, hasChanges }) => {
+}> = ({ newCratio, currentCRatio, targetCratio, liquidationCratio, hasChanges }) => {
   return (
     <CRatioBarUi
       liquidationCratio={liquidationCratio || 100}
       targetCratio={targetCratio || 100}
       currentCRatio={currentCRatio || 0}
       newCratio={newCratio}
-      isLoading={isLoading}
       hasChanges={hasChanges}
     />
   );
