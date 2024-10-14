bbf6bde4f18b6c8b6d3a2890ff3ef7682247cf4b
Noisekit <noisekit@proton.me>
2024-10-14 14:05:07 +1100

    JSON importers must only return JSON without wrapping into ethers.Contract (also future react-sdk compatibility)

--- liquidity/lib/useMulticall3/useMulticall3.ts
+++ liquidity/lib/useMulticall3/useMulticall3.ts
@@ -1,22 +1,21 @@
-import { Contract } from '@ethersproject/contracts';
 import { importMulticall3 } from '@snx-v3/contracts';
-import { Network, useNetwork, useProviderForChain } from '@snx-v3/useBlockchain';
+import { Network, useNetwork } from '@snx-v3/useBlockchain';
 import { useQuery } from '@tanstack/react-query';
 
 export function useMulticall3(customNetwork?: Network) {
   const { network } = useNetwork();
   const targetNetwork = customNetwork || network;
-  const provider = useProviderForChain(targetNetwork);
 
   return useQuery({
     queryKey: [`${targetNetwork?.id}-${targetNetwork?.preset}`, 'Multicall3'],
-    enabled: Boolean(provider && targetNetwork),
+    enabled: Boolean(targetNetwork),
     queryFn: async function () {
-      if (!(provider && targetNetwork)) throw new Error('OMFG');
+      if (!targetNetwork) throw new Error('OMFG');
 
-      const { address, abi } = await importMulticall3(targetNetwork.id, targetNetwork.preset);
-      return new Contract(address, abi, provider);
+      return importMulticall3(targetNetwork.id, targetNetwork.preset);
     },
     staleTime: Infinity,
+    // On some chains this is not available, and that is expected
+    throwOnError: false,
   });
 }
