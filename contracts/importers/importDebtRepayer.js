201eba2b607d9aa1d6ecbc3e6869563e92abdef0
Noisekit <noisekit@proton.me>
2024-10-21 17:58:37 +1100

    Disable Arbitrum DebtRepay

--- contracts/importers/importDebtRepayer.js
+++ contracts/importers/importDebtRepayer.js
@@ -13,14 +13,15 @@ export async function importDebtRepayer(chainId, preset) {
       // https://sepolia.basescan.org/address/0x0d08ff9e0ceddf81a85bc160d5d378eea7a1e200#code
       return { address: '0x0d08ff9e0ceddf81a85bc160d5d378eea7a1e200', abi };
     }
-    case '42161-main': {
-      // https://arbiscan.io/address/0x2305f5f9ef3abf0d6d02411aca44f85113b247af#code
-      return { address: '0x2305f5f9ef3abf0d6d02411aca44f85113b247af', abi };
-    }
-    case '421614-main': {
-      // https://sepolia.arbiscan.io/address/0xd88de2ee855f145ab1ecfff8273661c3d59fc8ad#code
-      return { address: '0xd88de2ee855f145ab1ecfff8273661c3d59fc8ad', abi };
-    }
+    // Arbitrum contracts cannot be the same as Base as the workflow is different
+    //    case '42161-main': {
+    //      // https://arbiscan.io/address/0x2305f5f9ef3abf0d6d02411aca44f85113b247af#code
+    //      return { address: '0x2305f5f9ef3abf0d6d02411aca44f85113b247af', abi };
+    //    }
+    //    case '421614-main': {
+    //      // https://sepolia.arbiscan.io/address/0xd88de2ee855f145ab1ecfff8273661c3d59fc8ad#code
+    //      return { address: '0xd88de2ee855f145ab1ecfff8273661c3d59fc8ad', abi };
+    //    }
     default: {
       throw new Error(`Unsupported deployment ${deployment} for DebtRepayer`);
     }
