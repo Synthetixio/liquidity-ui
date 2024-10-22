68d8ab0e488df1a3c01a5c56ba5c626b595cdd61
Noisekit <noisekit@proton.me>
2024-10-22 16:57:26 +1100

    Improve test coverage

--- contracts/close-position-arbitrum/src/ClosePosition.sol
+++ contracts/close-position-arbitrum/src/ClosePosition.sol
@@ -5,11 +5,8 @@ import {ISynthetixCore} from "./lib/ISynthetixCore.sol";
 import {IUSDToken} from "./lib/IUSDToken.sol";
 import {ERC2771Context} from "./lib/ERC2771Context.sol";
 
-/**
- * Tiny contract which deposits the needed amount of collateral into synthetix v3 account for debt repayment
- */
 contract ClosePosition {
-    error NotEnoughBalance(address tokenAddress, uint256 requiredAmount, uint256 availableAmount);
+    error NotEnoughBalance(address walletAddress, address tokenAddress, uint256 requiredAmount, uint256 availableAmount);
 
     function closePosition(
         address coreProxyAddress,
@@ -19,26 +16,29 @@ contract ClosePosition {
     ) public {
         address msgSender = ERC2771Context._msgSender();
         ISynthetixCore coreProxy = ISynthetixCore(coreProxyAddress);
+        // Get current debt for position
         int256 debt = coreProxy.getPositionDebt(accountId, poolId, collateralType);
         if (debt > 0) {
             uint256 debtAmount = uint256(debt);
 
             IUSDToken usdToken = IUSDToken(coreProxy.getUsdToken());
+            // Get deposited amount of USD Tokens available for repayment
             uint256 depositedAmount = coreProxy.getAccountAvailableCollateral(accountId, address(usdToken));
             if (debtAmount > depositedAmount) {
-                // Need to deposit more tokens
+                // Need to deposit more USD tokens
                 uint256 requiredAmount = debtAmount - depositedAmount;
                 uint256 availableAmount = usdToken.balanceOf(msgSender);
                 if (requiredAmount > availableAmount) {
-                    revert NotEnoughBalance(address(usdToken), requiredAmount, availableAmount);
+                    // Wallet does not have enough USD tokens to repay debt
+                    revert NotEnoughBalance(msgSender, address(usdToken), requiredAmount, availableAmount);
                 }
                 usdToken.approve(address(coreProxy), requiredAmount);
                 coreProxy.deposit(accountId, address(usdToken), requiredAmount);
             }
-            // Now we have more or exact amount of usd tokens deposited to repay the debt
+            // Now we have more or exact amount of USD tokens deposited to repay the debt
             coreProxy.burnUsd(accountId, poolId, collateralType, debtAmount);
         } else if (debt < 0) {
-            // claim negative debt
+            // Claim negative debt
             coreProxy.mintUsd(accountId, poolId, collateralType, uint256(-debt));
         }
 
