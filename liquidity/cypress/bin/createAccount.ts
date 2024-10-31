3593534a29f3e0777e67017f7d52bdf002c47b73
Noisekit <noisekit@proton.me>
2024-10-31 16:42:51 +1100

    Typecheck

--- liquidity/cypress/bin/createAccount.ts
+++ liquidity/cypress/bin/createAccount.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env ts-node
 
 import { createAccount } from '../cypress/tasks/createAccount';
-const [address] = process.argv.slice(2);
-if (!address) {
-  throw new Error('Usage: ./approveCollateral.ts <address>');
+const [address, accountId] = process.argv.slice(2);
+if (!address || !accountId) {
+  throw new Error('Usage: ./approveCollateral.ts <address> <accountId>');
 }
-createAccount({ address });
+createAccount({ address, accountId });
