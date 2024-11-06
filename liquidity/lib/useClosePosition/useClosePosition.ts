0d09b6a14fbbfa6634eee5a60777d0471a744751
Noisekit <noisekit@proton.me>
2024-11-06 13:38:25 +1100

    Update useClosePosition

--- liquidity/lib/useClosePosition/useClosePosition.ts
+++ liquidity/lib/useClosePosition/useClosePosition.ts
@@ -1,36 +1,20 @@
-import { Contract } from '@ethersproject/contracts';
 import { importClosePosition } from '@snx-v3/contracts';
-import {
-  Network,
-  useNetwork,
-  useProvider,
-  useProviderForChain,
-  useSigner,
-} from '@snx-v3/useBlockchain';
+import { Network, useNetwork } from '@snx-v3/useBlockchain';
 import { useQuery } from '@tanstack/react-query';
 
 export function useClosePosition(customNetwork?: Network) {
   const { network } = useNetwork();
-  const provider = useProvider();
-  const signer = useSigner();
-  const providerForChain = useProviderForChain(customNetwork);
-  const signerOrProvider = signer || provider;
   const targetNetwork = customNetwork || network;
-  const withSigner = Boolean(signer);
+
   return useQuery({
-    queryKey: [`${targetNetwork?.id}-${targetNetwork?.preset}`, 'ClosePosition', { withSigner }],
-    enabled: Boolean(signerOrProvider && targetNetwork),
+    queryKey: [`${targetNetwork?.id}-${targetNetwork?.preset}`, 'ClosePosition'],
+    enabled: Boolean(targetNetwork),
     queryFn: async function () {
-      if (!(signerOrProvider && targetNetwork)) throw new Error('OMFG');
-      if (providerForChain && customNetwork) {
-        const { address, abi } = await importClosePosition(targetNetwork.id, targetNetwork.preset);
-        return new Contract(address, abi, providerForChain);
-      }
-      const { address, abi } = await importClosePosition(targetNetwork?.id, targetNetwork?.preset);
-      return new Contract(address, abi, signerOrProvider);
+      if (!targetNetwork) throw 'OMFG';
+      return importClosePosition(targetNetwork.id, targetNetwork.preset);
     },
-    // we may not have this on all chains
-    throwOnError: false,
     staleTime: Infinity,
+    // On some chains this is not available, and that is expected
+    throwOnError: false,
   });
 }
