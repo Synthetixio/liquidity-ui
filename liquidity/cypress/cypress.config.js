5746281c28bdf98da41edb74789d52faa6681440
Noisekit <noisekit@proton.me>
2024-11-09 02:38:00 +1100

    Wait longer

--- liquidity/cypress/cypress.config.js
+++ liquidity/cypress/cypress.config.js
@@ -58,8 +58,8 @@ module.exports = defineConfig({
       openMode: 0,
     },
 
-    defaultCommandTimeout: 120_000,
-    execTimeout: 120_000,
-    taskTimeout: 120_000,
+    defaultCommandTimeout: 300_000,
+    execTimeout: 300_000,
+    taskTimeout: 300_000,
   },
 });
