6696cecaf59a997eff88b1c0f8220551aa82cb05
Noisekit <noisekit@proton.me>
2024-11-19 16:44:47 +1100

    Pretty

--- liquidity/cypress/cypress/e2e/8453-andromeda/Manage USDC Position - Deposit.e2e.js
+++ liquidity/cypress/cypress/e2e/8453-andromeda/Manage USDC Position - Deposit.e2e.js
@@ -39,7 +39,6 @@ it('Manage USDC Position - Deposit', () => {
     'exist'
   );
 
-  
   // TODO: Enable additional deposit after fixing an issue with balance refetching
   //
   //  cy.get('[data-cy="deposit amount input"]').should('exist').clear().type('69');
