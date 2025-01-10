cc42811a2129fa4d35abee79593a73e9a9a5f3e0
Noisekit <noisekit@proton.me>
2025-01-10 16:24:11 +1100

    Total rewards value

--- liquidity/ui/src/components/Stats/StatsList.tsx
+++ liquidity/ui/src/components/Stats/StatsList.tsx
@@ -2,8 +2,10 @@ import { Flex, Text } from '@chakra-ui/react';
 import { Amount } from '@snx-v3/Amount';
 import { DebtAmount, PnlAmount } from '@snx-v3/DebtAmount';
 import { useNetwork } from '@snx-v3/useBlockchain';
+import { useCollateralPrices } from '@snx-v3/useCollateralPrices';
 import { useLiquidityPositions } from '@snx-v3/useLiquidityPositions';
 import { useParams } from '@snx-v3/useParams';
+import { useRewards } from '@snx-v3/useRewards';
 import { wei } from '@synthetixio/wei';
 import React from 'react';
 import { StatBox } from './StatBox';
@@ -12,9 +14,53 @@ export const StatsList = () => {
   const [params] = useParams();
   const { network } = useNetwork();
 
-  const { data: liquidityPositions, isPending } = useLiquidityPositions({
+  const { data: rewards, isPending: isPendingRewards } = useRewards({
     accountId: params.accountId,
   });
+
+  const rewardsTokens = React.useMemo(() => {
+    const result: Set<string> = new Set();
+    if (rewards) {
+      for (const reward of rewards) {
+        if (reward.collateralType) {
+          result.add(reward.collateralType.address);
+        }
+      }
+    }
+    return result;
+  }, [rewards]);
+
+  const { data: rewardsTokenPrices, isPending: isPendingRewardsPrices } = useCollateralPrices(
+    rewardsTokens,
+    network
+  );
+
+  const totalRewardsValue = React.useMemo(
+    () =>
+      rewards
+        ? rewards.reduce(
+            (result, reward) =>
+              reward &&
+              reward.collateralType &&
+              reward.collateralType.address &&
+              rewardsTokenPrices &&
+              rewardsTokenPrices.has(reward.collateralType.address)
+                ? result.add(
+                    reward.claimableAmount.mul(
+                      rewardsTokenPrices.get(reward.collateralType.address)
+                    )
+                  )
+                : result,
+            wei(0)
+          )
+        : wei(0),
+    [rewards, rewardsTokenPrices]
+  );
+
+  const { data: liquidityPositions, isPending: isPendingLiquidityPositions } =
+    useLiquidityPositions({
+      accountId: params.accountId,
+    });
 
   const totalDebt = React.useMemo(
     () =>
@@ -57,7 +103,7 @@ export const StatsList = () => {
     <Flex flexWrap="wrap" w="100%" gap="4">
       <StatBox
         title="Available to Lock"
-        isLoading={Boolean(params.accountId && isPending)}
+        isLoading={Boolean(params.accountId && isPendingLiquidityPositions)}
         value={<Amount prefix="$" value={wei(totalAssets || '0')} />}
         label={
           <>
@@ -72,7 +118,7 @@ export const StatsList = () => {
 
       <StatBox
         title="Total Locked"
-        isLoading={Boolean(params.accountId && isPending)}
+        isLoading={Boolean(params.accountId && isPendingLiquidityPositions)}
         value={<Amount prefix="$" value={wei(totalLocked || '0')} />}
         label={
           <>
@@ -84,18 +130,36 @@ export const StatsList = () => {
       {network?.preset === 'andromeda' ? (
         <StatBox
           title="Total PNL"
-          isLoading={Boolean(params.accountId && isPending)}
-          value={<PnlAmount debt={totalDebt} />}
-          label={<Text textAlign="left">Aggregated PNL of all your open Positions</Text>}
+          isLoading={Boolean(
+            params.accountId &&
+              isPendingLiquidityPositions &&
+              isPendingRewards &&
+              isPendingRewardsPrices
+          )}
+          value={<PnlAmount debt={totalDebt.sub(totalRewardsValue)} />}
+          label={
+            <Text textAlign="left">
+              Aggregated PNL of all your open Positions and combined value of all your Rewards
+            </Text>
+          }
         />
       ) : null}
 
       {network?.preset !== 'andromeda' ? (
         <StatBox
           title="Total Debt"
-          isLoading={Boolean(params.accountId && isPending)}
-          value={<DebtAmount debt={totalDebt} />}
-          label={<Text textAlign="left">Aggregated Debt of all your open Positions</Text>}
+          isLoading={Boolean(
+            params.accountId &&
+              isPendingLiquidityPositions &&
+              isPendingRewards &&
+              isPendingRewardsPrices
+          )}
+          value={<DebtAmount debt={totalDebt.sub(totalRewardsValue)} />}
+          label={
+            <Text textAlign="left">
+              Aggregated value of all your Rewards minus your open Positions Debt
+            </Text>
+          }
         />
       ) : null}
     </Flex>
