f75c3e604945bfed54d1709ce410a1196400ea16
Noisekit <noisekit@proton.me>
2025-01-22 13:33:50 +1100

    Move all components to lib

---
+++ liquidity/lib/Migration/index.ts
@@ -0,0 +1,7 @@
+export * from './StepSuccessFinal';
+export * from './MigrationDialog';
+export * from './StepSummary';
+export * from './StepSuccess';
+export * from './MigrationBanner';
+export * from './StepIntro';
+export * from './StepExplain';
