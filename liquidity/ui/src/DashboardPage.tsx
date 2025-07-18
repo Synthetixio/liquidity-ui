5bb1b90a0ddb802bc3676bd2df8533334e3ae26a
cavalier-eth <91091124+cavalier-eth@users.noreply.github.com>
2025-07-18 13:39:27 +1000

    remove page content, and make alert persistent

--- liquidity/ui/src/DashboardPage.tsx
+++ liquidity/ui/src/DashboardPage.tsx
@@ -1,11 +1,10 @@
-import { Alert, AlertIcon, Collapse, Flex, Heading, Link, Text } from '@chakra-ui/react';
-import { MAINNET, OPTIMISM, BASE_ANDROMEDA, useNetwork, useWallet } from '@snx-v3/useBlockchain';
+import { Alert, AlertIcon, Collapse, Flex, Link, Text } from '@chakra-ui/react';
+import { useWallet } from '@snx-v3/useBlockchain';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useMigrationData } from './useMigrationData';
 
 export function DashboardPage() {
-  const { network } = useNetwork();
   const { activeWallet } = useWallet();
   const walletAddress = activeWallet?.address;
   const { data: migrationData } = useMigrationData({ walletAddress });
@@ -20,30 +19,21 @@ export function DashboardPage() {
           <Alert status="error" mb="6">
             <AlertIcon />
             <Text>
-              We’ve deprecated solo staking and your account was impacted. Please create a
+              We&apos;ve deprecated solo staking and your account was impacted. Please create a
               ticket in discord and the team will help recover your account.
             </Text>
           </Alert>
         </Collapse>
 
-        <Collapse
-          in={
-            !migrationData?.address && (network?.id === MAINNET.id || network?.id === OPTIMISM.id || BASE_ANDROMEDA.id})
-          }
-          animateOpacity
-          unmountOnExit
-        >
-          <Alert status="warning" mb="6">
-            <AlertIcon />
-            <Text>
-              Legacy positions on Ethereum, Optimism and Base have been refunded or{' '}
-              <Link isExternal href="https://420.synthetix.io" color="cyan.500">
-                migrated to 420.
-              </Link>
-            </Text>
-          </Alert>
-        </Collapse>
-        </Flex>
+        <Alert status="warning" mb="6">
+          <AlertIcon />
+          <Text>
+            Legacy positions on Ethereum, Optimism and Base have been refunded or{' '}
+            <Link isExternal href="https://420.synthetix.io" color="cyan.500">
+              migrated to 420.
+            </Link>
+          </Text>
+        </Alert>
       </Flex>
     </>
   );
